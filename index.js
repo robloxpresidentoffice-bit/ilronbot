917fc66d4a226c0b9d29b48ed3dda2b4b418f9f9
robloxpresidentoffice-bit <roblox.president.office@gmail.com>
2025-11-18 19:05:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ client.once("ready", async () => {
     }
   }
 
-  setInterval(checkVerifyReactions, 1000);
+  setInterval(checkVerifyReactions, 3000);
 });
 
 // === 닉네임 자동 업데이트 함수 ===
@@ -281,4 +281,5 @@ client.on("guildMemberRemove", async (member) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
 
